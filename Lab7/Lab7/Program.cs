c44594dab52065232f79534d422ec97505ad101f
Danila <danila.mazurenko2017@ya.ru>
2021-04-23 21:02:50 +0300

    💯💯💯

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static bool checkRat(string str)
+        {
+            string[] s_form = str.Split(' ');
+            for (int i = 0; i < s_form.Length; i++)
+            {
+                if (s_form[i] == "из")
+                    return true;
+            }
+                    for (int i = 0; i < str.Length; i++)
+            {
+                if (int.TryParse(str.Substring(i, 1), out int type) || str[i] == '.' || str[i] == ',' || str[i] == '/' || (str[i] == 'и' && str[i+1] == 'з'))
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+        static void checkPick(ref string str, int n)
+        {
+            bool isPicked = false;
+            while (!isPicked)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (str == i.ToString())
+                    {
+                        isPicked = true;
+                    }
+                }
+                if (isPicked == true)
+                {
+                    continue;
+                }
+                Console.WriteLine("Неверный ввод. Выберете действие из предложенных сверху: ");
+                str = Console.ReadLine();
+            }
+        }
+       static void Main(string[] args)
+        {
+
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("0. Представление дроби в разных форматах\n" +
+                              "1. Сложение дробей\n" +
+                              "2. Вычитание дробей\n" +
+                              "3. Умножение дробей\n" +
+                              "4. Деление дробей\n" +
+                              "5. Сравнение дробей");
+            string wtd = Console.ReadLine();
+            bool isPicked = false;
+            while(!isPicked)
+            {
+                if (wtd == "0" || wtd == "1" || wtd == "2" || wtd == "3" || wtd == "4" || wtd == "5")
+                {
+                    isPicked = true;
+                    continue;
+                }
+                Console.WriteLine("Неверный ввод. Выберете действие из предложенных сверху: ");
+                wtd = Console.ReadLine();
+            }
+            switch (wtd)
+            {
+                case "0":
+                    {
+                        Console.Write("Введите рациональное число: ");
+                        string _ratio = Console.ReadLine();
+                        while(!checkRat(_ratio))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio = Console.ReadLine();
+                        }
+                        Rational ratt = new Rational(_ratio);
+                        Console.Write("Выберите формат представления числа: ");
+                        Console.WriteLine("\n0. Десятичная дробь\n1. Обыкновенная дробь\n2. \"Специальный\" формат :)\n3. Приведение к целочисленному типу");
+                        string pick = Console.ReadLine();
+                        checkPick(ref pick, 4);
+                        ratt.chooseFormat(pick);
+                        if (pick == "3")
+                        {
+                            Console.WriteLine((int)ratt);
+                        }
+                        else
+                        {
+                            ratt.chooseFormat(pick);
+                            Console.WriteLine(ratt.ToString());
+                        }
+                        break;
+                    }
+                case "1":
+                    {
+                        Console.WriteLine("Введите первую дробь: ");
+                        string _ratio1 = Console.ReadLine();
+                        while (!checkRat(_ratio1))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio1 = Console.ReadLine();
+                        }
+                        Rational first = new Rational(_ratio1);
+                        Console.WriteLine("Введите вторую дробь: ");
+                        string _ratio2 = Console.ReadLine();
+                        while (!checkRat(_ratio2))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio2 = Console.ReadLine();
+                        }
+                        Rational second = new Rational(_ratio2);
+                        Rational result = first + second;
+                        Console.Write("Выберите формат представления числа: ");
+                        Console.WriteLine("\n0. Десятичная дробь\n1. Обыкновенная дробь\n2. \"Специальный\" формат :)");
+                        string pick = Console.ReadLine();
+                        checkPick(ref pick, 3);
+                        result.chooseFormat(pick);
+                        Console.WriteLine("Результат сложения: ");
+                        Console.WriteLine(result.ToString());
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Введите первую дробь: ");
+                        string _ratio1 = Console.ReadLine();
+                        while (!checkRat(_ratio1))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio1 = Console.ReadLine();
+                        }
+                        Rational first = new Rational(_ratio1);
+                        Console.WriteLine("Введите вторую дробь: ");
+                        string _ratio2 = Console.ReadLine();
+                        while (!checkRat(_ratio2))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio2 = Console.ReadLine();
+                        }
+                        Rational second = new Rational(_ratio2);
+                        Rational result = first - second;
+                        Console.Write("Выберите формат представления числа: ");
+                        Console.WriteLine("\n0. Десятичная дробь\n1. Обыкновенная дробь\n2. \"Специальный\" формат :)");
+                        string pick = Console.ReadLine();
+                        checkPick(ref pick, 3);
+                        result.chooseFormat(pick);
+                        Console.WriteLine("Результат вычетания: ");
+                        Console.WriteLine(result.ToString());
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.WriteLine("Введите первую дробь: ");
+                        string _ratio1 = Console.ReadLine();
+                        while (!checkRat(_ratio1))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio1 = Console.ReadLine();
+                        }
+                        Rational first = new Rational(_ratio1);
+                        Console.WriteLine("Введите вторую дробь: ");
+                        string _ratio2 = Console.ReadLine();
+                        while (!checkRat(_ratio2))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio2 = Console.ReadLine();
+                        }
+                        Rational second = new Rational(_ratio2);
+                        Rational result = first * second;
+                        Console.Write("Выберите формат представления числа: ");
+                        Console.WriteLine("\n0. Десятичная дробь\n1. Обыкновенная дробь\n2. \"Специальный\" формат (:))");
+                        string pick = Console.ReadLine();
+                        checkPick(ref pick, 3);
+                        result.chooseFormat(pick);
+                        Console.WriteLine("Результат умножения: ");
+                        Console.WriteLine(result.ToString());
+                        break;
+                    }
+                case "4":
+                    {
+                        Console.WriteLine("Введите первую дробь: ");
+                        string _ratio1 = Console.ReadLine();
+                        while (!checkRat(_ratio1))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio1 = Console.ReadLine();
+                        }
+                        Rational first = new Rational(_ratio1);
+                        Console.WriteLine("Введите вторую дробь: ");
+                        string _ratio2 = Console.ReadLine();
+                        while (!checkRat(_ratio2))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio2 = Console.ReadLine();
+                        }
+                        Rational second = new Rational(_ratio2);
+                        Rational result = first / second;
+                        Console.Write("Выберите формат представления числа: ");
+                        Console.WriteLine("\n0. Десятичная дробь\n1. Обыкновенная дробь\n2. \"Специальный\" формат (:))");
+                        string pick = Console.ReadLine();
+                        checkPick(ref pick, 3);
+                        result.chooseFormat(pick);
+                        Console.WriteLine("Результат деления: ");
+                        Console.WriteLine(result.ToString());
+                        break;
+                    }
+                case "5":
+                    {
+                        Console.WriteLine("Введите первую дробь: ");
+                        string _ratio1 = Console.ReadLine();
+                        while (!checkRat(_ratio1))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio1 = Console.ReadLine();
+                        }
+                        Rational first = new Rational(_ratio1);
+                        Console.WriteLine("Введите вторую дробь: ");
+                        string _ratio2 = Console.ReadLine();
+                        while (!checkRat(_ratio2))
+                        {
+                            Console.WriteLine("Неправильный тип числа. Повторите ввод: ");
+                            _ratio2 = Console.ReadLine();
+                        }
+                        Rational second = new Rational(_ratio2);
+                        Console.WriteLine("Результат: ");
+                        if (first == second)
+                        {
+                            Console.WriteLine($"{first.ToString()} = {second.ToString()}");
+                        }
+                        else if (first < second)
+                        {
+                            Console.WriteLine($"{first.ToString()} < {second.ToString()}");
+                        }
+                        else if (first > second)
+                        {
+                            Console.WriteLine($"{first.ToString()} > {second.ToString()}");
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Lab7/Lab7/Rational.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    class Rational : IComparable, IEquatable<Rational>
+    {
+        public int n;
+        public int m;
+        private string finalResult { get; set; }
+
+        public Rational(string num)
+        {
+            defineFormat(num);
+            finalResult = num;
+        }
+        public Rational(int a, int b)
+        {
+            n = a;
+            m = b;
+            finalResult = $"{n}/{m}";
+        }
+
+        public bool Equals(Rational other)
+        {
+            if (other.ToString() == null)
+                return false;
+            
+            if (this.m == other.m)
+                return true;
+            
+            return false;
+        }
+
+        public int CompareTo(object obj)
+        {
+            this.Contract();
+            if (obj == null) return 1;
+
+            Rational other = obj as Rational;
+            if (other.ToString() != null)
+                return (this.n * other.m).CompareTo(other.n * this.m);
+            else
+                return 2;
+        }
+        public void defineFormat(string num) //определение формата по введенной строке
+        {
+            int format = 2;  //по дефолту стоит 2 - для целочисленного типа
+            for(int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == '/')
+                {
+                    format = 0;
+                    break;
+                }
+                else if(num[i] == '.' || num[i] == ',')
+                {
+                    format = 1;
+                    break;
+                }
+            }
+
+            //если предыдущий код не нашел ни точки, ни запятой, ни знака деления, то дальше идет проверка на "специальный" формат
+            string[] s_form = num.Split(' ');
+            for (int i = 0; i < s_form.Length; i++)
+            {
+                if(s_form[i] == "из")
+                {
+                    if(int.TryParse(s_form[i-1], out int type))
+                    {
+                        n = int.Parse(s_form[i - 1]);
+                    } 
+                    else if (int.TryParse(s_form[i-2], out int type2))
+                    {
+                        n = int.Parse(s_form[i - 2]);
+                    }
+                    else if (int.TryParse(s_form[i - 3], out int type3))
+                    {
+                        n = int.Parse(s_form[i - 3]);
+                    }
+                    else
+                    {
+                        throw new Exception("Между первым числом и словом \"из\" не должно быть больше 2 слов."); //хз зачем Exception добавил
+                    }
+                    m = int.Parse(s_form[i + 1]);
+                    return;
+                }
+            }
+
+            //далее идет разбивка (если строка с точкой(запятой) или со знаком деления)
+            StringBuilder _temp = new StringBuilder();
+            int pos = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == '/' || num[i] == '.' || num[i] == ',')
+                {
+                    pos = i + 1;
+                    break;
+                }
+                else
+                {
+                    _temp.Append(num[i]);
+                }
+            }
+            string temp = _temp.ToString();
+            int a = int.Parse(temp);   //первая часть строки, ДО точки(запятой) или знака деления
+
+            StringBuilder _temp2 = new StringBuilder();
+            for (int i = pos; i < num.Length; i++)
+            {
+                _temp2.Append(num[i]);
+            }
+            string temp2 = _temp2.ToString();
+            int b = int.Parse(temp2); //ну и соотвественно та часть строки после точки(запятой) или знака деления
+
+            if (format == 0) //если строка содержит знак деления
+            {
+                n = a;
+                m = b;                
+            } 
+            else if (format == 1) //если была введена десятичная дробь
+            {
+                int pdten = (int)Math.Pow(10, temp2.Length);
+                if (a > 0)  
+                {
+                    n = b + (pdten * a);
+                } else
+                {
+                    n = b;
+                }
+                m = pdten;
+                chooseFormat("0");
+            } 
+            else //если было введено целое число
+            {
+               n = a;
+               m = 1;
+            }
+        }
+        public int chooseFormat(string pick)
+        {
+            Contract();
+            switch(pick)
+            {
+                case "0": finalResult = ((double) n / m).ToString(); break;
+                case "2": finalResult = $"Всемирный конгресс математиков оценивает данную дробь на {n} из {m}"; break; 
+                default: finalResult = $"{n}/{m}"; break;
+            }
+            return 0;
+        }
+
+        public void Contract()
+        {
+            for (int i = 2; i < 10; i++)
+            {
+                if (n % i == 0 && m % i == 0)
+                {
+                    n = n / i;
+                    m = m / i;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return finalResult;
+        }
+
+        public static Rational operator +(Rational a, Rational b)
+        {
+            if(a.m == b.m)
+            {
+                return new Rational(a.n + b.n, a.m);
+            } 
+            else
+            {
+                return new Rational((a.n * b.m) + (a.m * b.n), a.m * b.m);
+            }
+        }
+
+        public static Rational operator -(Rational a, Rational b)
+        {
+            if (a.m == b.m)
+            {
+                return new Rational(a.n - b.n, a.m);
+            }
+            else
+            {
+                return new Rational((a.n * b.m) - (a.m * b.n), a.m * b.m);
+            }
+        }
+
+        public static Rational operator *(Rational a, Rational b)
+        {
+            return new Rational(a.n * b.n, a.m * b.m);
+        }
+
+        public static Rational operator /(Rational a, Rational b)
+        {
+            return new Rational(a.n * b.m, a.m * b.n);
+        }
+
+        public static Rational operator -(Rational a)
+        {
+            return new Rational(-a.n, a.m);
+        }
+
+        public static Rational operator ++(Rational a)
+        {
+            return new Rational(a.n + a.m, a.m);
+        }
+
+        public static bool operator >(Rational a, Rational b)
+        {
+            int _t = a.CompareTo(b);
+            if (_t == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(Rational a, Rational b)
+        {
+            int _t = a.CompareTo(b);
+            if (_t == -1)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        public static bool operator ==(Rational a, Rational b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Rational a, Rational b)
+        {
+            return !a.Equals(b);
+        }
+
+        public static explicit operator int(Rational a)
+        {
+            double _d = (double)a;
+            double mod = _d - Math.Truncate(_d);
+            if(mod >= 0.5)
+            {
+                return (a.n / a.m) + 1;
+            } 
+            else
+            {
+                return a.n / a.m;
+            }
+        }
+
+        public static implicit operator Rational(double a)
+        {
+            return new Rational(a.ToString());
+        }
+
+        public static implicit operator Rational(int a)
+        {
+            return new Rational(a, 1);
+        }
+
+        public static explicit operator double(Rational a)
+        {
+            return (double)(a.n / a.m);
+        }
+    }
+}
